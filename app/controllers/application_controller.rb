5752e0d8694b3b9a6199507019c0d9d314f1ef61
shoutm <shouta.morimoto@gmail.com>
2012-11-25 17:30:12 +0900

    Add jquery mobile js and css. Divide layout into two(for pc and smartphone)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
+require 'ruby-debug'
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  layout :set_layout
   before_filter :login_check
+
+  def set_layout
+    smartphone_request? ? "smartphone" : "application"
+  end
 
   def login_check
     # TODO openid での認証
     @user = User.find(:first, conditions: ["email = :email", {email: "user1@test.com"}])   
   end
+
+  private 
+  def smartphone_request?
+    request.user_agent =~ /(Mobile.+Safari)/
+  end
+
 end
