0a6e3e27d7c4cced774af6587126bfc54f97d73f
shoutm <shouta.morimoto@gmail.com>
2013-01-20 01:04:52 +0900

    Make friend apply notices to show at portal#index.

--- app/controllers/portal/index_controller.rb
+++ app/controllers/portal/index_controller.rb
@@ -8,21 +8,24 @@ class Portal::IndexController < ApplicationController
 
 
   private 
-  ### インナークラス
 
+  ####################
+  # Inner classes    #
+  ####################
   class Notice
     attr_accessor :msg, :uri
   end
 
-
-
-  ### プライベートメソッド
-
+  ####################
+  # Private methods  #
+  ####################
   # == 概要
   # ユーザに対する通知がある場合
   def get_notices
     notices = []
     notice = get_playing_competition_notice
+    notices << notice unless notice.nil?
+    notice = get_friend_apply_notice
     notices << notice unless notice.nil?
     return notices
   end
@@ -40,6 +43,22 @@ class Portal::IndexController < ApplicationController
     notice = Notice.new
     notice.msg = "プレイ中のコンペがあります"
     notice.uri = "/competition/play/index?competition_id=" + player.party.competition_id.to_s
+    return notice
+  end
+
+  # == 概要
+  # ゴルフレ申請がある場合、通知を返却する
+  #
+  # == 返り値
+  # ゴルフレ申請がある場合: Noticeインスタンス
+  # ゴルフレ申請がない場合: nil
+  def get_friend_apply_notice
+    fr = FriendRelation.find(:first, conditions: {:"user_id" => @user.id, state: FriendRelation::State::BE_REQUESTED})
+    return nil if fr.nil?
+
+    notice = Notice.new
+    notice.msg = "ゴルフレ申請があります。"
+    notice.uri = "/portal/friend/show?user_id=" + fr.friend_id.to_s
     return notice
   end
 end
